d35688e94d7aa23380bb1632155cd977e02ce6ec
Priyanshu Chand <priyanshu.pchand@gmail.com>
2022-12-21 12:30:17 +0530

    committing ...

--- Program6/src/Main.java
+++ Program6/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
+// Fibonacci series in Java without recursion
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Enter how many fibonacci number you want to print");
+        Scanner in = new Scanner(System.in);
+        int count = in.nextInt();
+        int a=0, b=1, fibNum;
+        System.out.print(a+" "+b);
+        for (int i = 2; i < count; i++) {
+            fibNum = a+b;
+            System.out.print(" "+fibNum);
+            a = b;
+            b = fibNum;
+        }
     }
 }
